f66c5faf82e8a2dbdb7d0d764b571b8df5452ed7
akoumari <alex.koumarianos@gmail.com>
2022-02-28 07:19:27 -0500

    MVP

---
+++ src/components/Grid.js
@@ -0,0 +1,156 @@
+import React, { useEffect, useMemo, useState } from "react";
+import useKeypress from "react-use-keypress";
+import {
+  getPlayerInput,
+  newFrame,
+  snakeSnacks,
+  newFood,
+  DEFAULT_SNAKE,
+  DEFAULT_SPEED,
+  DEFAULT_INPUT,
+  DEFAULT_GRID,
+} from "../logic/snakeLogic";
+import useInterval from "@use-it/interval";
+const Grid = () => {
+  const [snake, setSnake] = useState(DEFAULT_SNAKE);
+  const [input, setInput] = useState(DEFAULT_INPUT);
+  const [speed, setSpeed] = useState(DEFAULT_SPEED);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(Number(localStorage.getItem("score")));
+  const [hasWalls, setHasWalls] = useState(true);
+  const [food, setFood] = useState(newFood(snake));
+  const [pauseFood, setPauseFood] = useState(false);
+  const [wait, setWait] = useState(false);
+  const [grid, setGrid] = useState(DEFAULT_GRID);
+  useKeypress(
+    ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown", " "],
+    (event) => {
+      event.preventDefault();
+
+      if (!wait) {
+        setWait(true);
+        getPlayerInput(event.key, snake, input, setInput, setSnake);
+        setWait(false);
+      }
+    }
+  );
+  useEffect(() => {
+    console.log(snake);
+    setGrid([
+      ...grid.map((row) => {
+        let inner = row.map((col) => {
+          return {
+            ...col,
+            hasSnk:
+              snake.body.filter((part) => part.x == col.x && part.y == col.y)
+                ?.length >= 1,
+            hasFood: food.x == col.x && food.y == col.y,
+            hasHead: snake.head.x == col.x && snake.head.y == col.y,
+          };
+        });
+        return [...inner];
+      }),
+    ]);
+  }, [snake, food]);
+  useEffect(() => {
+    const { x: headX, y: headY } = snake.head;
+    const { x, y } = food;
+
+    if (headX == x && headY == y && !pauseFood) {
+      setPauseFood(true);
+      setFood(newFood(snake));
+      snakeSnacks(snake, setSnake);
+    }
+    setPauseFood(false);
+  }, [snake]);
+  useInterval(() => {
+    if (!snake.pause && !snake.gameOver)
+      newFrame(snake, setSnake, input, setInput, hasWalls);
+  }, speed);
+  const gridUI = useMemo(() => {
+    console.log("change snake");
+
+    return (
+      <div className={`grid-container`}>
+        {grid.map((row) => {
+          return row.map((col) => {
+            return (
+              <div
+                key={`(${col.x}, ${col.y})`}
+                className={`grid-item ${col.hasFood ? "food" : ""} ${
+                  col.hasSnk ? "snake-body" : ""
+                }  ${col.hasHead ? "snake-head" : ""}`}
+              ></div>
+            );
+          });
+        })}
+      </div>
+    );
+  }, [grid, snake]);
+  useEffect(() => {
+      setScore((snake.body.length - 2) * 100)
+    }, [snake])
+    
+    useEffect(() => {
+       if(score>highScore){
+           localStorage.setItem("score", score)
+           setHighScore(score)
+       }
+      }, [score])
+  return (
+    <div className={``}>
+      <div className="sidebar">
+      <div>
+          
+        {snake.gameOver ? "GAME OVER" : `Score: ${score}`}
+          </div> 
+      <div>
+        High Score: {highScore}
+          </div> 
+        <button
+          className={`mode mb ${hasWalls ? "active-mode" : ""}`}
+          onClick={() => {
+            setHasWalls(!hasWalls);
+          }}
+        >
+          Walls: {hasWalls?"ON":"OFF"}
+        </button>
+        <button
+          className={`mode ${speed == 150 ? "active-mode" : ""}`}
+          onClick={() => {
+            setSpeed(150);
+          }}
+        >
+          easy
+        </button>
+        <button
+          className={`mode ${speed == 100 ? "active-mode" : ""}`}
+          onClick={() => {
+            setSpeed(DEFAULT_SPEED);
+          }}
+        >
+          medium
+        </button>
+        <button
+          className={`mode ${speed == 50 ? "active-mode" : ""}`}
+          onClick={() => {
+            setSpeed(50);
+          }}
+        >
+          hard
+        </button>
+        <button
+          className={`mode ${speed == 10 ? "active-mode" : ""}`}
+          onClick={() => {
+            setSpeed(10);
+          }}
+        >
+          GOD MODE
+        </button>
+      </div>
+      {gridUI}
+    </div>
+  );
+};
+
+export default Grid;
---
+++ src/logic/snakeLogic.js
@@ -0,0 +1,163 @@
+const xAxis = new Map([
+  ["ArrowLeft", -1],
+  ["ArrowRight", 1],
+]);
+const yAxis = new Map([
+  ["ArrowUp", -1],
+  ["ArrowDown", 1],
+]);
+const checkGameOver = (snake, newSnake, setSnake, hasWalls) => {
+  if (
+    snake.body
+      .slice(2)
+      .filter((part) => part.x == snake.head.x && part.y == snake.head.y)
+      .length >= 1
+  ) {
+    return setSnake({ ...newSnake, gameOver: true });
+  }
+  if(hasWalls&&(Math.abs(snake.head.x-newSnake.head.x)>1||Math.abs(snake.head.y-newSnake.head.y)>1)){
+    return setSnake({ ...snake, gameOver: true });
+  }
+  return setSnake(newSnake);
+};
+const moveBody = (body, head) => {
+  let newbody = [...body];
+  for (let i = body.length - 1; i > 0; i--) {
+    newbody[i] = newbody[i - 1];
+  }
+  newbody[0] = head;
+  return newbody;
+};
+const getNext = (next) => {
+  if (next > 49) return 0;
+  if (next < 0) return 49;
+  return next;
+};
+const moveX = (key, snake, setSnake, input, setInput, hasWalls) => {
+  let change = xAxis.get(key);
+  setInput({ ...input, waitOnFrame: false });
+  let newbody = moveBody(snake.body, { x: snake.head.x, y: snake.head.y });
+  let newSnake = {
+    ...snake,
+    body: newbody,
+
+    pause: false,
+    moving: key,
+    head: {
+      ...snake.head,
+      x: getNext(snake.head.x + change),
+      nextX: snake.head.x + 2 * change,
+    },
+  };
+  return checkGameOver(snake, newSnake, setSnake, hasWalls);
+};
+const moveY = (key, snake, setSnake, input, setInput, hasWalls) => {
+  let change = yAxis.get(key);
+  setInput({ ...input, waitOnFrame: false });
+  let newbody = moveBody(snake.body, { x: snake.head.x, y: snake.head.y });
+  let newSnake = {
+    ...snake,
+    body: newbody,
+    pause: false,
+    moving: key,
+    head: {
+      ...snake.head,
+      y: getNext(snake.head.y + change),
+      nextY: snake.head.y + 2 * change,
+    },
+  };
+
+  return checkGameOver(snake, newSnake, setSnake, hasWalls);
+};
+const pause = (snake, setSnake) => {
+  if (snake.gameOver) return setSnake(DEFAULT_SNAKE);
+  setSnake({ ...snake, pause: !snake.pause });
+};
+const getValidAction = (key, snake, input, setInput) => {
+  if (xAxis.has(key) && !xAxis.has(snake.moving) && !input.waitOnFrame) {
+    return setInput({ moving: key, waitOnFrame: true });
+  }
+  if (yAxis.has(key) && !yAxis.has(snake.moving) && !input.waitOnFrame) {
+    console.log(key);
+    return setInput({ moving: key, waitOnFrame: true });
+  }
+};
+
+export const newFrame = (snake, setSnake, input, setInput, hasWalls) => {
+  const { moving } = input;
+  if (xAxis.has(moving)) {
+    return moveX(moving, snake, setSnake, input, setInput, hasWalls);
+  }
+  if (yAxis.has(moving)) {
+    console.log(moving);
+    return moveY(moving, snake, setSnake, input, setInput, hasWalls);
+  }
+};
+export const snakeSnacks = (snake, setSnake) => {
+  let newbody = [...snake.body];
+  newbody.push(newbody[newbody.length - 1]);
+  let newSnake = {
+    ...snake,
+    body: newbody,
+  };
+  return setSnake(newSnake);
+};
+export const newFood = (snake) => {
+  let food = {
+    x: Math.floor(Math.random() * 50),
+    y: Math.floor(Math.random() * 50),
+  };
+  while (
+    snake.body.filter((part) => part.x == food.x && part.y == food.y).length >=
+    1
+  ) {
+    food = {
+      x: Math.floor(Math.random() * 51),
+      y: Math.floor(Math.random() * 51),
+    };
+  }
+  return food;
+};
+
+export const DEFAULT_SPEED = 100;
+export const DEFAULT_SNAKE = {
+  head: {
+    x: 25,
+    y: 25,
+    nextX: 26,
+    nextY: 25,
+  },
+  moving: "ArrowLeft",
+  pause: true,
+  waitOnFrame: false,
+  gameOver: false,
+  length: 2,
+  body: [...Array(2)].map((_, j) => {
+    return {
+      x: 25,
+      y: 25,
+    };
+  }),
+};
+export const DEFAULT_INPUT = {
+  moving: "ArrowLeft",
+  waitOnFrame: false,
+};
+export const DEFAULT_GRID = [...Array(50)].map((_, i) => {
+    let inner = [...Array(50)].map((_, j) => {
+      return {
+        hasSnk: false,
+        hasFood: false,
+        x: i,
+        y: j,
+        isEdge: false,
+        moveTo: null,
+        hasHead: false,
+      };
+    });
+    return inner;
+  })
+export const getPlayerInput = (key, snake, input, setInput, setSnake) => {
+  if (key == " ") return pause(snake, setSnake);
+  return getValidAction(key, snake, input, setInput);
+};
